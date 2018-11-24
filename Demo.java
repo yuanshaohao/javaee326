17dc40cee6bad48b3f89b7d34a5cf7e4212a4798
123 <123@163.com>
2018-11-24 18:14:56 +0800

    添加demo

---
+++ Demo.java
@@ -0,0 +1,23 @@
+public class Content implements Serializable {
+    private Long id;
+
+    /**
+     * 内容类目ID
+     */
+    private Long categoryId;
+
+    /**
+     * 内容标题
+     */
+    private String title;
+
+    /**
+     * 链接
+     */
+    private String url;
+
+    /**
+     * 图片绝对路径
+     */
+    private String pic;
+}
